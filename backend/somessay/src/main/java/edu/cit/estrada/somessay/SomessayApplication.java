e6e0dc6aa365302c0003f21180db875aa56882a1
carmel-estrada28 <carmelestradaa@gmail.com>
2026-03-09 09:54:08 +0800

    setup(backend): started setting up spring boot project with supabase connection and database tables

---
+++ backend/somessay/src/main/java/edu/cit/estrada/somessay/SomessayApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.estrada.somessay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomessayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomessayApplication.class, args);
+	}
+
+}
---
+++ backend/somessay/src/test/java/edu/cit/estrada/somessay/SomessayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.estrada.somessay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomessayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
