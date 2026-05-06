08c55fcb0b760b3bb1cec808e85382bace88450e
carmel-estrada28 <carmelestradaa@gmail.com>
2026-05-06 14:24:41 +0800

    feat(web): update routing with protected routes and new pages

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,17 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import OAuth2Redirect from "./pages/OAuth2Redirect";
-import Home from "./pages/Home"
+import OAuth2Redirect from './pages/OAuth2Redirect'
+import Home from './pages/Home'
+import Feed from './pages/Feed'
+import CreateArticle from './pages/CreateArticle'
+import ArticleDetail from './pages/ArticleDetail'
+import Profile from './pages/Profile'
+import Landing from './pages/Landing'
+import Activity from './pages/Activity' 
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem('token')
+  return token ? children : <Navigate to="/login" />
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/oauth2/redirect" element={<OAuth2Redirect />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/feed" element={<ProtectedRoute><Feed /></ProtectedRoute>} />
+        <Route path="/create" element={<ProtectedRoute><CreateArticle /></ProtectedRoute>} />
+        <Route path="/article/:id" element={<ProtectedRoute><ArticleDetail /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/activity" element={<ProtectedRoute><Activity /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   )
